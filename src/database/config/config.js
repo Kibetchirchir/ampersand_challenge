e8b217d3da075774c91f5d4bdda86fc223497621
chirchir <langatchirchir@gmail.com>
2022-07-10 08:25:57 +0200

    - initiate node js boiler plate

---
+++ src/database/config/config.js
@@ -0,0 +1,20 @@
+const config  = {
+  "development": {
+    "use_env_variable": "DATABASE_URL_DEV",
+    "dialect": "postgres",
+    "logging": false
+  },
+  "test": {
+    "use_env_variable": "DATABASE_URL_TEST",
+    "dialect": "postgres",
+    "logging": false
+  },
+  "production": {
+    "logging": false,
+    "use_env_variable": "DATABASE_URL_PRODUCTION",
+   "dialect": "postgres"
+  }
+}
+
+module.exports = config;
+
